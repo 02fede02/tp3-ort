b5b42a02e6bce386722b99dd2ecdef25f85621b2
02fede02 <fede.birman93@gmail.com>
2023-05-28 18:31:38 -0300

    feat/cliente

---
+++ tp3/src/ort/edu/ar/tp3/ej6/Cliente.java
@@ -0,0 +1,25 @@
+package ort.edu.ar.tp3.ej6;
+
+import java.util.ArrayList;
+
+public class Cliente {
+	private String dni;
+	private String nombre;
+	private double saldoPagar;
+	private ArrayList<Pelicula> historialPeliculas;
+	private Servicio suscripcion;
+	
+	public Cliente(String dni, String nombre, Servicio suscripcion) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.saldoPagar = 0;
+		this.suscripcion = suscripcion;
+	}
+	
+	public boolean esDeudor() {
+		return saldoPagar > 0;
+	}
+	
+	
+}
