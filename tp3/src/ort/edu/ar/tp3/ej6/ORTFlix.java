5497c1d4a14b72e56f6d41c97d9329e58e022f2e
02fede02 <fede.birman93@gmail.com>
2023-05-29 13:35:45 -0300

    fix/ORTFlix

--- tp3/src/ort/edu/ar/tp3/ej6/ORTFlix.java
+++ tp3/src/ort/edu/ar/tp3/ej6/ORTFlix.java
@@ -13,8 +13,55 @@ public class ORTFlix {
 		this.listaNegra = new ArrayList<Cliente>();
 	}
 	
+	private Cliente buscarCliente(String dni) {
+		Cliente clienteEncontrado = null;
+		int i = 0;
+		
+		while(i < clientes.size() && clienteEncontrado == null) {
+			Cliente clienteActual = clientes.get(i);
+			
+			if(clienteActual.getDni().equals(dni)) {
+				clienteEncontrado = clienteActual;
+			} else {
+				i++;
+			}
+		}
+		return clienteEncontrado;
+	}
+	
+	private Pelicula buscarPelicula(String nombre) {
+		Pelicula peliculaEncontrada = null;
+		int i = 0;
+		
+		while(i < catalogo.size() && peliculaEncontrada== null) {
+			Pelicula peliculaActual = catalogo.get(i);
+			
+			if(peliculaActual.getNombre().equals(nombre)) {
+				peliculaEncontrada = peliculaActual;
+			} else {
+				i++;
+			}
+		}
+		return peliculaEncontrada;
+	}
+	
 	public OperacionVerPelicula verPelicula(String dni, String pelicula) {
 		OperacionVerPelicula estado = null;
+		Cliente cliente = buscarCliente(dni);
+		Pelicula peliculaVer = buscarPelicula(pelicula);
+		
+		if(cliente == null) {
+			estado = OperacionVerPelicula.CLIENTE_INEXISTENTE;
+		} else if(peliculaVer == null) {
+			estado = OperacionVerPelicula.CONTENIDO_INEXISTENTE;
+		} else if(cliente.esDeudor()) {
+			estado = OperacionVerPelicula.CLIENTE_DEUDOR;
+		} else if(!peliculaVer.getCategoria().equals(cliente.getSuscripcion())) {
+			estado = OperacionVerPelicula.CONTENIDO_NO_DISPONIBLE;
+		} else {
+			estado = OperacionVerPelicula.OK;
+			cliente.agregarPelicula(peliculaVer);
+		}
 		
 		return estado;
 	}
