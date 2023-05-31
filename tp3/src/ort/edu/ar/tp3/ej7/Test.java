53d4f441cb4464a6c25c4996644d2a39e78789d0
02fede02 <fede.birman93@gmail.com>
2023-05-31 17:32:28 -0300

    fix/Test

--- tp3/src/ort/edu/ar/tp3/ej7/Test.java
+++ tp3/src/ort/edu/ar/tp3/ej7/Test.java
@@ -4,7 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		Aplicacion app = new Aplicacion();
 		app.altaCancion("Roundabout", "Yes", "Rock");
-		System.out.println(app.altaUsuario("fede.@gmail.com", 30, Categoria.PREMIUM));
+		app.altaCancion("Shadowplay", "Joy Division", "Rock");
+		System.out.println(app.altaUsuario("fede.@gmail.com", 30, Categoria.GRATUITO));
 		System.out.println(app.altaUsuario("fedeee.@gmail.com", 30, Categoria.PREMIUM));
 		
 		System.out.println(app.escucharCancion("fede.@gmail.com", "Roundabout", "Yes"));
@@ -14,5 +15,10 @@ public class Test {
 		app.listarUsuarios();
 		app.listarCancionesPorUsuario("fede.@gmail.com");
 		app.listarCancionesPorUsuario("fede.@gmail.coma");
+		
+		for (int i = 0; i < 51; i++) {
+			app.escucharCancion("fede.@gmail.com", "Roundabout", "Yes");
+		}
+		System.out.println(app.escucharCancion("fede.@gmail.com", "Shadowplay", "Joy Division"));
 	}
 }
