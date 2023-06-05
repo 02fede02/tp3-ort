630db649c3998034ee2c72595cbf0533974ae255
02fede02 <fede.birman93@gmail.com>
2023-06-04 22:06:25 -0300

    feat/Piloto-methods

--- tp3/src/ort/edu/ar/tp3/ej11/Piloto.java
+++ tp3/src/ort/edu/ar/tp3/ej11/Piloto.java
@@ -3,6 +3,7 @@ package ort.edu.ar.tp3.ej11;
 public class Piloto {
 	private String nombre;
 	private String dni;
+	private int cantVueltas;
 	private double tiempoVuelta;
 	private double sumaTiempo;
 	private double promedioVueltas;
@@ -10,13 +11,21 @@ public class Piloto {
 	public Piloto(String nombre, String dni) {
 		this.nombre = nombre;
 		this.dni = dni;
+		this.cantVueltas = 0;
 		this.tiempoVuelta = 0;
 		this.sumaTiempo = 0;
 		this.promedioVueltas = 0;
 	}
 	
 	public void calcularVueltaPromedio() {
-		
+		if(this.cantVueltas > 0 ) {
+			this.promedioVueltas = this.sumaTiempo / this.cantVueltas;
+		} else {
+			System.out.println("El piloto no realizo ninguna vuelta");
+		}
 	}
 	
+	public String toString() {
+		return this.nombre + " " + this.dni;
+	}
 }
